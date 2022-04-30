d8e15f68d7d5d712bb3d3f5934be99eeb1139be5
Robel <rofikre@yahoo.com>
2022-04-30 15:07:15 -0400

    add single park page and map functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,68 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Container, Grid } from "@mui/material";
 
 // react router
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 // apollo
 import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  HttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+	HttpLink
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 // components
-import Nav from './components/Nav';
-import Footer from './components/Footer';
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 // pages
-import Home from './pages/Home';
-import Dashboard from './pages/Dashboard';
-import SinglePark from './pages/SinglePark';
-import NoMatch from './pages/NoMatch';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import SinglePark from "./pages/SinglePark";
+import NoMatch from "./pages/NoMatch";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import { useState } from "react";
 
 const httpLink = new HttpLink({
-  uri: '/graphql',
+	uri: "/graphql"
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
+	const token = localStorage.getItem("id_token");
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : ""
+		}
+	};
 });
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
+	link: authLink.concat(httpLink),
+	cache: new InMemoryCache()
 });
 
 function App() {
-  return (
-    <ApolloProvider client={client}>
-      <Router>
-        <div>
-            <Nav />
-            <Routes>
-              <Route exact path="/" element={<Home />} />
-              <Route exact path="/login" element={<Login />} />
-              <Route exact path="/signup" element={<Signup />} />
-              <Route exact path="/dashboard" element={<Dashboard />} />
-              <Route exact path="/park/:id" element={<SinglePark />} />
-              <Route element={<NoMatch/>}></Route>
-            </Routes>
-            <Footer />
-        </div>
-      </Router>
-    </ApolloProvider>
-  );
+	const [parkClicked, setParkClicked] = useState("");
+	return (
+		<ApolloProvider client={client}>
+			<Router>
+				<Container className={"top_60"}>
+					<Grid item xs={12} sm={12} md={4} lg={3}>
+						<Nav />
+					</Grid>
+					<Grid item xs={12} sm={12} md={4} lg={3}>
+						<Routes>
+							<Route
+								exact
+								path="/"
+								element={
+									<Home
+										parkClicked={parkClicked}
+										setParkClicked={setParkClicked}
+									/>
+								}
+							/>
+							<Route exact path="/login" element={<Login />} />
+							<Route exact path="/signup" element={<Signup />} />
+							<Route exact path="/dashboard" element={<Dashboard />} />
+							<Route exact path="/park/:id" element={<SinglePark />} />
+							<Route element={<NoMatch />}></Route>
+						</Routes>
+					</Grid>
+					<Grid item xs={12} sm={12} md={4} lg={3}>
+						<Footer />
+					</Grid>
+				</Container>
+			</Router>
+		</ApolloProvider>
+	);
 }
 
 export default App;
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,42 +1,169 @@
-import { Button } from '@material-ui/core';
-import Auth from '../../utils/auth';
-import { Link } from 'react-router-dom';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import { Link } from "react-router-dom";
+import Auth from "../../utils/auth";
+
+const pages = ["Dashboard"];
+const settings = ["Logout"];
 
 const Nav = () => {
-  function showNavigation() {
-    if (Auth.loggedIn()) {
-      return (
-        <a href='/' onClick={() => {Auth.logout()}}>
-          <Button variant="contained">Logout</Button>
-        </a>
-      );
-    } else {
-      return (
-        <ul>
-          <li>
-            <Link to="/login">
-              <Button variant="contained">Login</Button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/signup">
-              <Button variant="contained">Sign Up</Button>
-            </Link>
-          </li>
-        </ul>
-      );
-    }
-  }
+	const [anchorElNav, setAnchorElNav] = React.useState(null);
+	const [anchorElUser, setAnchorElUser] = React.useState(null);
+	if (Auth.loggedIn()) {
+		const handleOpenNavMenu = (event) => {
+			setAnchorElNav(event.currentTarget);
+		};
+		const handleOpenUserMenu = (event) => {
+			setAnchorElUser(event.currentTarget);
+		};
 
-  return (
-    <header>
-      <h1>
-        <Link to="/">National Park Spotter</Link>
-      </h1>
+		const handleCloseNavMenu = () => {
+			setAnchorElNav(null);
+		};
 
-      <nav>{showNavigation()}</nav>
-    </header>
-  );
+		const handleCloseUserMenu = () => {
+			setAnchorElUser(null);
+		};
+
+		return (
+			<AppBar position="static">
+				<Container maxWidth="xl">
+					<Toolbar disableGutters>
+						<Typography
+							variant="h6"
+							noWrap
+							component="div"
+							sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
+						>
+							<Link to="/">National Park Spotter</Link>
+						</Typography>
+
+						<Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+							<IconButton
+								size="large"
+								aria-label="account of current user"
+								aria-controls="menu-appbar"
+								aria-haspopup="true"
+								onClick={handleOpenNavMenu}
+								color="inherit"
+							>
+								<MenuIcon />
+							</IconButton>
+							<Menu
+								id="menu-appbar"
+								anchorEl={anchorElNav}
+								anchorOrigin={{
+									vertical: "bottom",
+									horizontal: "left"
+								}}
+								keepMounted
+								transformOrigin={{
+									vertical: "top",
+									horizontal: "left"
+								}}
+								open={Boolean(anchorElNav)}
+								onClose={handleCloseNavMenu}
+								sx={{
+									display: { xs: "block", md: "none" }
+								}}
+							>
+								{pages.map((page) => (
+									<MenuItem key={page} onClick={handleCloseNavMenu}>
+										<Link to={page} textAlign="center">
+											{page}
+										</Link>
+									</MenuItem>
+								))}
+							</Menu>
+						</Box>
+						<Typography
+							variant="h6"
+							noWrap
+							component="div"
+							sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+						>
+							<Link to="/">National Park Spotter</Link>
+						</Typography>
+						<Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+							{pages.map((page) => (
+								<Link
+									to={page}
+									key={page}
+									onClick={handleCloseNavMenu}
+									sx={{ my: 2, color: "white", display: "block" }}
+								>
+									{page}
+								</Link>
+							))}
+						</Box>
+
+						<Box sx={{ flexGrow: 0 }}>
+							<Tooltip title="Open settings">
+								<IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+									<Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+								</IconButton>
+							</Tooltip>
+							<Menu
+								sx={{ mt: "45px" }}
+								id="menu-appbar"
+								anchorEl={anchorElUser}
+								anchorOrigin={{
+									vertical: "top",
+									horizontal: "right"
+								}}
+								keepMounted
+								transformOrigin={{
+									vertical: "top",
+									horizontal: "right"
+								}}
+								open={Boolean(anchorElUser)}
+								onClose={handleCloseUserMenu}
+							>
+								{settings.map((setting) => (
+									<MenuItem key={setting} onClick={handleCloseUserMenu}>
+										<Link
+											to={"/"}
+											textAlign="center"
+											onClick={() => {
+												Auth.logout();
+											}}
+										>
+											{setting}
+										</Link>
+									</MenuItem>
+								))}
+							</Menu>
+						</Box>
+					</Toolbar>
+				</Container>
+			</AppBar>
+		);
+	} else {
+		return (
+			<ul>
+				<li>
+					<Link to="/login">
+						<Button variant="contained">Login</Button>
+					</Link>
+				</li>
+				<li>
+					<Link to="/signup">
+						<Button variant="contained">Sign Up</Button>
+					</Link>
+				</li>
+			</ul>
+		);
+	}
 };
-
 export default Nav;
