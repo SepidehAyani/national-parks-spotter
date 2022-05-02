31ede9699a6fc1491a74c9634816bbc42de584c4
C Bares <colinedwinbares@gmail.com>
2022-05-02 14:17:50 -0500

    clean up dev

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Container, Grid } from "@mui/material";
 
--- server/models/Reaction.js
+++ server/models/Reaction.js
@@ -1,5 +1,4 @@
 const { Schema } = require("mongoose");
-const dateFormat = require("../utils/dateFormat");
 
 const reactionSchema = new Schema(
 	{
